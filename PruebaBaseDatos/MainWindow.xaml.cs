e01db34e7ee461c7caa42493ad520d390add56e7
AbelCasanovaMontalban <abel.casanova@iesdoctorbalmis.com>
2020-01-14 12:40:05 +0100

    Añadido DataGrid y Filtrar

--- PruebaBaseDatos/MainWindow.xaml.cs
+++ PruebaBaseDatos/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace PruebaBaseDatos
     public partial class MainWindow : Window
     {
         private DatabaseAbelCasanovaEntities contexto;
+        private CollectionViewSource vista;
 
         public MainWindow()
         {
@@ -33,11 +34,41 @@ namespace PruebaBaseDatos
 
             contexto = new DatabaseAbelCasanovaEntities();
             contexto.CLIENTES.Load();
+            contexto.PEDIDOS.Load();
 
             ClientesListBox.DataContext = contexto.CLIENTES.Local;
             IdComboBox.DataContext = contexto.CLIENTES.Local;
             IdModificarComboBox.DataContext = contexto.CLIENTES.Local;
+            DatosDataGrid.DataContext = contexto.CLIENTES.Local;
+             
 
+            vista = new CollectionViewSource();
+            vista.Source= contexto.CLIENTES.Local;
+            PedidosDataGrid.DataContext = vista;
+
+            vista.Filter += Vista_Filter;
+
+        }
+
+        private void Vista_Filter(object sender, FilterEventArgs e)
+        {
+            CLIENTE item = (CLIENTE)e.Item;
+
+            if (FiltrarTextBox.Text=="")
+            {
+                e.Accepted = true;
+            } else
+            {
+                String nombreCompleto = item.nombre + " " + item.apellido;
+                if (nombreCompleto.Contains(FiltrarTextBox.Text))
+                {
+                    e.Accepted = true;
+                }
+                else
+                {
+                    e.Accepted = false;
+                }
+            }
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
@@ -54,12 +85,25 @@ namespace PruebaBaseDatos
 
             CLIENTE nuevo = new CLIENTE();
             InsertarStackPanel.DataContext = nuevo;
+            
 
         }
 
         private void ModificarButton_Click(object sender, RoutedEventArgs e)
         {
             contexto.SaveChanges();
+        }
+
+        private void ActualizarButton_Click(object sender, RoutedEventArgs e)
+        {
+            contexto.SaveChanges();
+        }
+
+
+
+        private void FiltrarButton_Click(object sender, RoutedEventArgs e)
+        {
+            vista.View.Refresh();
         }
     }
 }
